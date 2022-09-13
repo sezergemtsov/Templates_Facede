b00518ab74b5baa7c87ffa0a1e7a83703276a961
sezergemtsov <sezer.gemtsov@ya.ru>
2022-09-13 19:24:34 +0300

    initial commit

---
+++ src/main/java/BinOps.java
@@ -0,0 +1,18 @@
+public class BinOps {
+
+    int a, b, r;
+
+    public String sum(String a, String b) {
+        this.a = Integer.parseInt(a, 2);
+        this.b = Integer.parseInt(b, 2);
+        r = this.a + this.b;
+        return Integer.toBinaryString(r);
+    }
+
+    public String mult(String a, String b) {
+        this.a = Integer.parseInt(a);
+        this.b = Integer.parseInt(b);
+        r = this.a * this.b;
+        return Integer.toBinaryString(r);
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        String a = "101";
+        String b = "10";
+        System.out.println(bins.sum(a, b));
+        System.out.println(bins.mult(a, b));
+    }
+}
